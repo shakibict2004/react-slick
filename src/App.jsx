2df18cc0e560b97c3d5a4a7b6f0890a662eb4990
shakib <shakibict@gmail.com>
2024-08-29 01:37:10 -0600

    first

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+
+import { Col, Container, Row } from 'react-bootstrap'
+import './App.css'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import ban from "../../shakib/src/assets/gaza.jpeg"
+import ban1 from "../../shakib/src/assets/gaza1.jpeg"
+import ban2 from "../../shakib/src/assets/gaza2.jpeg"
+import ban3 from "../../shakib/src/assets/gaza3.jpeg"
+function App() {
+  var settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1
+};
+  return (
+    <Slider {...settings}>
+
+  <div className="pick">
+    <img src={ban} alt="" />
+    <h2>pray for Philistine</h2>
+    <p></p>
+  </div>
+  <div className="pick">
+   <img src={ban1} alt="" />
+   <h2>pray for Gaza</h2>
+   <p></p>
+  </div>
+  <div className="div">
+    <img src={ban2} alt="" />
+    <h2>pray for Gaza</h2>
+    <p></p>
+  </div>
+  <div className="div">
+   <img src={ban3} alt="" />
+   <h2>pray for Gaza</h2>
+   <p></p>
+  </div>
+
+</Slider>
+  )
+}
+
+export default App
